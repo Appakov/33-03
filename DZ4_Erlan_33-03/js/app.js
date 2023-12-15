c88593e32a381aecee37382322c265a444f0741f
Appakov <147263500+Appakov@users.noreply.github.com>
2023-12-14 17:29:36 -1200

    clean

--- DZ4_Erlan_33-03/js/app.js
+++ DZ4_Erlan_33-03/js/app.js
@@ -2,10 +2,8 @@ var zero = document.querySelector('.zero')
 var plus = document.querySelector('.plus')
 var minus = document.querySelector('.minus')
 var reset = document.querySelector('.reset')
-zero.style.color = 'grey'
 
-plus.addEventListener('click', function () {
-    zero.value++
+function renderColor() {
     if (zero.value === 0) {
         zero.style.color = 'grey'
     } else if (zero.value > 0) {
@@ -15,31 +13,19 @@ plus.addEventListener('click', function () {
     } else {
         zero.style.color = 'grey'
     }
+}
 
+plus.addEventListener('click', function () {
+    zero.value++
+    renderColor()
 
 })
 minus.addEventListener('click', function () {
     zero.value--
-    if (zero.value === 0) {
-        zero.style.color = 'grey'
-    } else if (zero.value > 0) {
-        zero.style.color = 'green'
-    } else if (zero.value < 0) {
-        zero.style.color = 'red'
-    } else {
-        zero.style.color = 'grey'
-    }
+    renderColor()
 
 })
 reset.addEventListener('click', function () {
     zero.value = 0
-    if (zero.value === 0) {
-        zero.style.color = 'grey'
-    } else if (zero.value > 0) {
-        zero.style.color = 'green'
-    } else if (zero.value < 0) {
-        zero.style.color = 'red'
-    } else {
-        zero.style.color = 'grey'
-    }
+    renderColor()
 })
